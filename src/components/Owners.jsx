6684fda8e850f2a560d115ef82e239c0cb95bf00
Danny Evans <d.evans.18@unimail.winchester.ac.uk>
2024-03-14 11:38:58 +0000

    _

--- src/components/Owners.jsx
+++ src/components/Owners.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Owners() {
     const [ownerName, setOwnerName] = useState("")
     const [dogName, setDogName] = useState("")
     const [dogBreed, setDogBreed] = useState("")
+    const [data, setData] = useState()
 
     function AddDog(){
         var dogPost = {
@@ -19,8 +20,42 @@ function Owners() {
 
         fetch(("https://2v8hb1nh4g.execute-api.eu-north-1.amazonaws.com/Beta/Dog"), dogPost)
         .then((response) => response.json())
-        .then((data) => {console.log(data)})
+        .then((data2) => {console.log(data2)})
+        .then(() => window.location.reload(true))
+
     }
+
+    useEffect(() => {
+        fetch(("https://2v8hb1nh4g.execute-api.eu-north-1.amazonaws.com/Beta/Dog"), {method: "GET"})
+        .then((response) => response.json())
+        .then(json => setData(json["body"]))
+        .catch(error => console.log(error))
+    }, [])
+
+    function dogTable(data) {
+        return (
+            <table>
+            <thead>
+                <tr bgcolor = "DarkGray">
+                    <th>Owner Name</th>
+                    <th>Dog Name</th>
+                    <th>Breed</th>
+                </tr>
+                {data.map((val, key) => {
+                    return(
+                        <tr key={key}>
+                            <td>{val.ownerName}</td>
+                            <td>{val.dogName}</td>
+                            <td>{val.dogBreed}</td>
+                        </tr>
+                    )
+                })}
+            </thead>
+        </table>
+        )
+        
+    }
+
     
     return (
         <>
@@ -34,13 +69,9 @@ function Owners() {
         <input type="text" id="breeds" name="breeds" onChange={(e) => setDogBreed(e.target.value)}></input>
         <button onClick={() => AddDog()}>Submit</button>
         <h2>Currently Registered Dogs</h2>
-        <table bgcolor="Black">
-  <tr bgcolor = "DarkGray">
-    <th>Owner Name</th>
-    <th>Dog Name</th>
-    <th>Breed</th>
-  </tr>
-</table>
+        
+        {data ? dogTable(data) : <h1>Loading</h1>}
+        
         </>
     )
 }
--- src/components/Walkers.jsx
+++ src/components/Walkers.jsx
@@ -1,23 +1,76 @@
+import { useEffect, useState } from "react";
+
 function Walkers() {
+
+  const [walkerName, setWalkerName] = useState("")
+  const [workDays, setWorkDays] = useState("")
+  const [workTime, setWorkTime] = useState("")
+  const [data, setData] = useState()
+
+  function AddWalker(){
+      var walkerPost = {
+          method : "POST",
+          body: JSON.stringify(
+              {
+                  "walkerName": walkerName,
+                  "workDays": workDays,
+                  "workTime": workTime
+              }
+          )
+      }
+
+      fetch(("https://2v8hb1nh4g.execute-api.eu-north-1.amazonaws.com/Beta/Owner"), walkerPost)
+      .then((response) => response.json())
+      .then((data) => {console.log(data)})
+      .then(() => window.location.reload(true))
+  }
+
+  useEffect(() => {
+    fetch(("https://2v8hb1nh4g.execute-api.eu-north-1.amazonaws.com/Beta/Owner"), {method: "GET"})
+    .then((response) => response.json())
+    .then(json => setData(json["body"]))
+    .catch(error => console.log(error))
+}, [])
+
+function walkerTable(data) {
+  return (
+      <table>
+      <thead>
+      <tr bgcolor="DarkGray">
+    <th>Walker Name</th>
+    <th>Working Days</th>
+    <th>Working Time</th>
+  </tr>
+          {data.map((val, key) => {
+              return(
+                  <tr key={key}>
+                      <td>{val.walkerName}</td>
+                      <td>{val.workDays}</td>
+                      <td>{val.workTime}</td>
+                  </tr>
+              )
+          })}
+      </thead>
+  </table>
+  )
+  
+}
+
     return (
         <>
         <h1>Walkers</h1>
         <h2>Add New Walker</h2>
         <label for="fname">Full name:</label>
-        <input type="text" id="fname" name="fname"></input>
+        <input type="text" id="fname" name="fname"  onChange={(e) => setWalkerName(e.target.value)}></input>
         <label for="workdays">Available days:</label>
-        <input type="text" id="workdays" name="workdays"></input>
+        <input type="text" id="workdays" name="workdays"  onChange={(e) => setWorkDays(e.target.value)}></input>
         <label for="worktimes">Available times:</label>
-        <input type="text" id="worktimes" name="worktimes"></input>
-        <button>Submit</button>
+        <input type="text" id="worktimes" name="worktimes"  onChange={(e) => setWorkTime(e.target.value)}></input>
+        <button onClick={() => AddWalker()}>Submit</button>
         <h2>Currently Registered Walkers</h2>
-        <table bgcolor="Black">
-  <tr bgcolor="DarkGray">
-    <th>Walker Name</th>
-    <th>Working Days</th>
-    <th>Working Time</th>
-  </tr>
-</table>
+
+        {data ? walkerTable(data) : <h1>Loading</h1>}
+
         </>
     )
 }
